088f0fdc69ea3e187545face46847d73b41c2696
Nicholas Christensen <nickachristensen@gmail.com>
2024-08-22 16:08:04 -0400

    Update gardenManagement.js

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,40 +1,4 @@
-// Write your code here
-
-
-
-//This code is here to make sure our tests work properly. Don't remove or modify this.
-module.exports = {
-    get soilMoisture() {
-      return soilMoisture;
-    },
-    set soilMoisture(value) {
-      soilMoisture = value;
-    },
-    get isRaining() {
-      return isRaining;
-    },
-    set isRaining(value) {
-      isRaining = value;
-    },
-    get wateringStatus() {
-      return wateringStatus;
-    },
-    set wateringStatus(value) {
-      wateringStatus = value;
-    },
-    get timeOfDay() {
-      return timeOfDay;
-    },
-    set timeOfDay(value) {
-      timeOfDay = value;
-    },
-    get lightStatus() {
-      return lightStatus;
-    },
-    set lightStatus(value) {
-      lightStatus = value;
-    },
-    ...(typeof controlWatering === 'function' && { controlWatering }),
-    ...(typeof controlLights === 'function' && { controlLights }),
-    ...(typeof adjustSoilMoisture === 'function' && { adjustSoilMoisture })
-  };
+const temperature = 90;
+const timeOfDay = "morning";
+let soilMoisture = 30;
+const gardenTasks = ["Water plants", "Trim hedges", "Mow lawn"];
