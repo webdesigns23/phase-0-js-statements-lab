e5ea88d7c93f7d9a05d68c6b32df4f8851beb631
Sharmaine Perea <sharmaine.perea23@gmail.com>
2025-05-09 12:36:17 -0600

    added statements for water, light, soil

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,23 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+//Watering Control
+if (temperature > 80){
+    console.log("Watering On");
+} else {
+    console.log("Watering off");
+}
+
+//Garden Lights Control
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("Lights on");
+} else {
+    console.log("Lights off");
+}
+
+//Soil Moisture Adjustment
+while (soilMoisture <= 40) {
+    console.log("Soil Moisture:", soilMoisture);
+    soilMoisture += 5;
+}
